65e5d22213342be0c96e6e3ab15ba867cbcbce7a
AngeloDalicandro <angelo.dalicandro1@gmail.com>
2022-06-21 16:53:04 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,162 @@
+ const allIcons  = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+const cardContainer = document.querySelector('.card-container');
+
+printCardsDom(allIcons, cardContainer);
+
+const typeSelector = document.getElementById('icon-type');
+
+typeSelector.addEventListener('change',
+	function() {
+		// Resetto il container
+		cardContainer.innerHTML = '';
+		
+		const thisType = this.value;
+
+		if( thisType === 'all') {
+			printCardsDom(allIcons, cardContainer);
+		} else {
+			cardFilter(allIcons, cardContainer, thisType)
+		}
+	}
+)
+
+
+
+
+// FUNZIONI
+
+function printCardsDom (anArray, aContainer) {
+	anArray.forEach((thisIcon) => {
+		const {name, prefix, type, family, color} = thisIcon;
+	
+		const newCard = `
+		<div class="card color-${color}">
+			<i class="${family} ${prefix}${name}"></i>
+			<span>${name}</span>
+		</div>	
+		`
+		aContainer.innerHTML += newCard;
+	})
+}
+
+function cardFilter (anArray, aContainer, aFilter) {
+	const filteredArray = anArray.filter((thisIcon) => {
+		return thisIcon.type === aFilter
+	});
+
+	printCardsDom(filteredArray, aContainer);
+}
